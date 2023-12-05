b0f5559a30b655d1863552637ad1542874aee8c4
Denisrrr <denisrianov@gmail.com>
2023-12-05 15:24:44 +0500

    11.15

--- js/fetch.js
+++ js/fetch.js
@@ -1,25 +1,24 @@
 const Urls = {
-    GET: 'https://26.javascript.pages.academy/kekstagram/data',
-    POST: 'https://26.javascript.pages.academy/kekstagram',
-  };
-  
-  const sendRequest = (onSuccess, onFail, method, body) => {
-    fetch(
-      Urls[method],
-      {
-        method: method,
-        body: body,
-      },
+  GET: 'https://26.javascript.pages.academy/kekstagram/data',
+  POST: 'https://26.javascript.pages.academy/kekstagram',
+};
+const sendRequest = (onSuccess, onFail, method, body) => {
+  fetch(
+    Urls[method],
+    {
+      method: method,
+      body: body,
+    },
     )
-      .then((response) => response.json())
-      .then((data) => {
-        onSuccess(data);
-      })
-      .catch(() => {
-        onFail();
-      });
+    .then((response) => response.json())
+    .then((data) => {
+      onSuccess(data);
+    })
+    .catch(() => {
+      onFail();
+    });
   };
-  
-  export {sendRequest};
+
+export {sendRequest};
 
   
--- js/number.js
+++ js/number.js
@@ -1,17 +1,17 @@
 const numDecline = (num, nominative, genitiveSingular, genitivePlural) => {
-    const hundredths = num%100;
-    if(hundredths  <= 4 || hundredths  > 20){
-      const tenths = num%10;
-      if(tenths === 1){
-        return nominative;
-      }
-      if(tenths >= 2 && tenths <= 4){
-        return genitiveSingular;
-      }
+  const hundredths = num%100;
+  if(hundredths  <= 4 || hundredths  > 20){
+    const tenths = num%10;
+    if(tenths === 1){
+      return nominative;
     }
-    return genitivePlural;
-  };
-  
-  export {numDecline};
+    if(tenths >= 2 && tenths <= 4){
+      return genitiveSingular;
+    }
+  }
+  return genitivePlural;
+};
+
+export {numDecline};
 
   
